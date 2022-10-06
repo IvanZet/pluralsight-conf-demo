32daec1ad448b32bec0ccd7a74b72d66aa2af795
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-10-06 11:06:31 +0200

    Hardcodes DB credentials in Java conf file

--- src/main/java/com/prularsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/prularsight/conferencedemo/config/PersistenceConfiguration.java
@@ -12,21 +12,12 @@ import javax.sql.DataSource;
  */
 @Configuration
 public class PersistenceConfiguration {
-    @Value("${DB_URL}")
-    private String dbUrl;
-
-    @Value("${DB_USERNAME}")
-    private String dbUsername;
-
-    @Value("${DB_PASSWORD}")
-    private String dbPassword;
-
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url(dbUrl);
-        builder.username(dbUsername);
-        builder.password(dbPassword);
+        builder.url("jdbc:postgresql://localhost:5432/conference_app");
+        builder.username("postgres");
+        builder.password("Welcome");
         System.out.println("My custom data source bean has been initialized and set");
         return builder.build();
     }
